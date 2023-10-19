2721cf1f25e38ba5ff978a637e245d183b57c52c
Александр <Volosnikov.aleksandr2@gmail.com>
2023-10-19 18:28:56 +0500

    Добавил объектов БД, обновил БД
    Связь юзеры-гильдии, результаты дамагера недели

--- DB_Entity/DBConnect.cs
+++ DB_Entity/DBConnect.cs
@@ -15,8 +15,11 @@ namespace GOD_Assistant.DB_Entity
         public DbSet<Guild> Guilds { get; set; } = null!;
         public DbSet<Role> Roles { get; set; } = null!;
         public DbSet<User_Role> User_Role { get; set; } = null!;
+        public DbSet<Guild_User> Guilds_Users { get; set; } = null!;
         public DbSet<VoiceLog> VoiceLogs { get; set; } = null!;
         public DbSet<Attachment> Attachments { get; set; } = null!;
+        public DbSet<DamagerResult> DamagerResults { get; set; } = null!;
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Filename=GOD.db");
